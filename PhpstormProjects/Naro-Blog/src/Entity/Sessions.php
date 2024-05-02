51f5383f9685e54f14fe028f7f257dbdffad05ae
yousef <youssefnaro@gmail.com>
2024-05-02 18:25:42 +0200

    second commit

---
+++ PhpstormProjects/Naro-Blog/src/Entity/Sessions.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SessionsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SessionsRepository::class)]
+class Sessions
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'sessions')]
+    private ?utilisateur $user_id = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $start_time = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $end_time = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $ip_address = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?utilisateur
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(?utilisateur $user_id): static
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getStartTime(): ?\DateTimeInterface
+    {
+        return $this->start_time;
+    }
+
+    public function setStartTime(\DateTimeInterface $start_time): static
+    {
+        $this->start_time = $start_time;
+
+        return $this;
+    }
+
+    public function getEndTime(): ?\DateTimeInterface
+    {
+        return $this->end_time;
+    }
+
+    public function setEndTime(\DateTimeInterface $end_time): static
+    {
+        $this->end_time = $end_time;
+
+        return $this;
+    }
+
+    public function getIpAddress(): ?string
+    {
+        return $this->ip_address;
+    }
+
+    public function setIpAddress(string $ip_address): static
+    {
+        $this->ip_address = $ip_address;
+
+        return $this;
+    }
+}
